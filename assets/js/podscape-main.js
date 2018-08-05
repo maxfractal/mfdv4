fd67dd8684c8a395ac3f6f5cde2e0e3b973afc46
Xris <cp@maxfractal.com>
2018-08-05 15:48:37 -0400

    Commit local work from last 6 months, index and landing

---
+++ assets/js/podscape-main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Xris on 9/27/2015.
+ */
+
+$(function() {
+	//cache the window object
+	var $window = $(window);
+	
+	//Parallax background effect
+	$('section[data-type="background"]').each(function() {
+		var $bgobj = $(this); //assigning the object
+
+		$(window).scroll(function() {
+			//scroll the background at var speed
+			//the yPos is a negative value
+
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+			//put together our final background position
+			var coords = '50% ' + yPos + 'px';
+
+			//move the background
+			$bgobj.css({ backgroundPoosition: coords});
+		})
+	});
+});
